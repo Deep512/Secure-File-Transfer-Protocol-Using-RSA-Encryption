db99e4f92b4b4cce017d1767c5c842e79cb7ff41
Deep512 <42740971+Deep512@users.noreply.github.com>
2019-11-13 19:19:52 +0530

    Update client.c

--- Client/client.c
+++ Client/client.c
@@ -1,18 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include<math.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<sys/fcntl.h>
-#include<arpa/inet.h>
-#include<unistd.h>
-#include<signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/fcntl.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
 #define MIN 16500
 #define MAX 32500
-
+#define PORT 4444
 
 long int generate_primes(long int prime_array[])
 {
@@ -82,8 +82,6 @@ printf("PHI(n) =%10ld ( 0x%lx )\n\nNOTE:phi(n) = phi(p)*phi(q) = (p-1)*(q-1)\n",
 //Thus, e is coprime to phi(n).(i.e. gcd(e,phi(n)) = 1)
 //So, the public key is declared as: (e,n)
 long int PUBLIC_KEY_EXPONENT=65537,PRIVATE_KEY_EXPONENT;
-//FILE *fp;
-//fp=fopen("public_key.txt","w+");
 PRIVATE_KEY_EXPONENT=mul_inv(PUBLIC_KEY_EXPONENT,PHI);
 printf("\nThe Public Key(e,n) is: ( %ld ( 0x%lx ), %ld ( 0x%lx ) )\n",PUBLIC_KEY_EXPONENT,PUBLIC_KEY_EXPONENT,MODULUS,MODULUS);
 printf("\nThe Private Key(d,n) is: ( %ld ( 0x%lx ), %ld ( 0x%lx ) )\n",PRIVATE_KEY_EXPONENT,PRIVATE_KEY_EXPONENT,MODULUS,MODULUS);
@@ -112,9 +110,9 @@ long long int modular_expo(long int b,long int e,long int m)
 
 void decryption(long int PRIVATE_EXPO,long int MOD,char *encrypted_str,int n,char *filename)
 {
-    long int encrypted_values[5000],decrypted_values[5000],i,j,value;
+    long int encrypted_values[100000],decrypted_values[100000],i,j,value;
     long long int decrypted_value;
-    char decrypted_msg[5000],value_str[20]={'\0'}, *endptr;
+    char decrypted_msg[100000],value_str[20]={'\0'}, *endptr;
 	FILE *fp=fopen(filename,"w+");
     //printf("\n\nEncrypted str:%s",encrypted_str);
     for(i=0,j=0;i<n;i++,j++){
@@ -132,65 +130,33 @@ void decryption(long int PRIVATE_EXPO,long int MOD,char *encrypted_str,int n,cha
 	fclose(fp);
 }
 
-int main(int argc, char *argv[])
+int main(int argc,char **argv)
 {
-	int sockfd, portno,n;
-	long int pub_expo,pri_expo,mod;
-	struct sockaddr_in serv_addr;
-	char buffer[256], *servip,pub_expo_str[256]={'\0'},mod_str[256]={'\0'},filename[256]={'\0'},content[50000]={'\0'};
-	if(argc<4)
-	   {
-		printf("Not enough parameters.");
-		exit(0);
-	   }
-	servip=argv[1] ;   //server ip is being delievered
-	portno=atoi(argv[3]); //port number is being delievered
-	strcpy(filename,argv[2]);	//requested file name is being kept and stored
-	
-	//Key generation is being done here
-	key_generate(&pri_expo,&pub_expo,&mod);
-	
-	//Converting long integer public key into string to append it after the filename
-	sprintf(pub_expo_str,"%ld",pub_expo);   
-	sprintf(mod_str,"%ld",mod);
-
-	strcat(argv[2],"#");	strcat(argv[2],pub_expo_str);  //argv[2] = filename + pub_expo_str, now
-	strcat(argv[2],"#");	strcat(argv[2],mod_str);   //argv[2] = filename + pub_expo_str + mod_str, now
-
-	sockfd=socket(AF_INET, SOCK_STREAM,0);
-	if(sockfd<0)
-		perror("error opening socket");
-	printf("client online\n");
-	bzero((char *)&serv_addr,sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(servip);
-	serv_addr.sin_port = htons(portno);
-	if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
-		perror("error connecting");
-
-	//to request file 'filename' we must write its name in buffer, So	
-	//write argv[2](filename + pub_expo_str + mod_str) in the buffer
-	write(sockfd,argv[2],strlen(argv[2])+1);	
-	
-	//file received by this point, So we need to read data
-	n=read(sockfd,content,50000);	//moves content from sockfd --> 'content[]'
-	
-	printf("Content Received:\n");	
-	write(1,content,n);     //write content of n length from 'content[]' to the terminal
-	if(n<=0)
-	  { 
-		perror("file not found");
-	 	exit(0);
-	   }
-
-/***********************************************
-Decryption function goes here   
-************************************************/
-
-decryption(pri_expo,mod,content,n,filename);
-
-/*************************************************/	
-
-printf("File Received.\n");		
-return 0;
+long int pub_expo,pri_expo,mod;
+char pub_expo_str[256]={'\0'},mod_str[256]={'\0'},filename[256]={'\0'},content[100000]={'\0'},f[256]={'\0'};
+int i,j;
+ssize_t n;
+struct sockaddr_in servaddr;
+int sockfd;
+key_generate(&pri_expo,&pub_expo,&mod);
+sprintf(pub_expo_str,"%ld",pub_expo);   
+sprintf(mod_str,"%ld",mod);
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+bzero(&servaddr,sizeof(servaddr));
+servaddr.sin_family=AF_INET;
+servaddr.sin_port=htons(PORT);
+inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+connect(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+printf("\nenter the filename:");
+scanf("%s",filename);
+printf("\n");
+strcpy(f,filename);
+strcat(filename,"#");	strcat(filename,pub_expo_str);  //argv[2] = filename + pub_expo_str, now
+strcat(filename,"#");	strcat(filename,mod_str);   //argv[2] = filename + pub_expo_str + mod_str, now
+write(sockfd,filename,sizeof(filename)+1);
+n=read(sockfd,content,50000);
+printf("Data from Server:\n");
+write(1,content,n); 
+decryption(pri_expo,mod,content,n,f);
+close(sockfd);
 }
