3469ad11f736dcd1b22d19b37deb688eb651b731
Deep512 <42740971+Deep512@users.noreply.github.com>
2019-11-16 18:02:19 +0530

    Update server.c

--- Server/server.c
+++ Server/server.c
@@ -9,8 +9,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <ctype.h>
-#include<arpa/inet.h>
-#define PORT 4444
+#include <arpa/inet.h>
 
 long int modular_expo(long int b,long int e,long int m)
 {
@@ -31,37 +30,38 @@ char * encrypt(long int PUBLIC_EXPO,long int MOD,char *filename)
     long int unencrypted_msg[50000],encrypted_msg[50000];
 	int i,str_len=0;
     char str[50000]={'\0'}, encrypted_file[]="encrypt.txt";
-
-    //Reading Public Key
+    FILE *erase_fp;
+    erase_fp=fopen(encrypted_file,"w+");
+    fclose(erase_fp);
     FILE *fp;
     fp=fopen(filename,"r");
-    printf("pub_expo:%ld\n",PUBLIC_EXPO);   //Reading Public Key.
-    printf("mod:%ld\n",MOD);
-    if(!fp)
-        printf("Error opening file!\n");    
-
-    //fscanf(fp,"%s",str);
-	printf("\nContent of file:\n\n");
-    fgets(str,10000,fp);
-    fputs(str,stdout);
+    if(!fp){
+        perror("file not found");
+        exit(0);
+    }
+    printf("pub_expo:%ld\n",PUBLIC_EXPO);   
+    printf("mod:%ld\n",MOD);    
+    printf("\nEncrypted Message:\n\n");
+    while(fgets(str,10000,fp))
+    {
     str_len=strlen(str);
-	
     for(i=0;i<str_len;i++)
     {	
 		unencrypted_msg[i]=(long int)str[i];
 	}
-    printf("\nEncrypted Message:\n\n");
     for(i=0;i<str_len;i++)
         encrypted_msg[i]=modular_expo(unencrypted_msg[i],PUBLIC_EXPO,MOD);
-	fclose(fp);
+
     FILE *encrypt_fp;
-    encrypt_fp=fopen(encrypted_file,"w+");
+    encrypt_fp=fopen(encrypted_file,"a+");
     for(i=0;i<str_len;i++)
         {
             printf("%ld:",encrypted_msg[i]);
             fprintf(encrypt_fp,"%ld\n",encrypted_msg[i]);
         }
-    fclose(encrypt_fp);
+        fclose(encrypt_fp);
+    }
+    fclose(fp);
     filename = encrypted_file;
 }
 
@@ -71,7 +71,7 @@ void always_on (int sock)
 	char buffer[256],requested_file[256]={'\0'},pub_expo_str[50]={'\0'},mod_str[50]={'\0'},*endptr,*file_name;
 	
 	bzero(buffer,256) ;
-	n=read(sock,buffer,256);	//Will receive content sent by client i.e. filename + public key
+	n=read(sock,buffer,256);
 
 	for(i=0;buffer[i]!='#';i++)
 		requested_file[i]=buffer[i];	
@@ -81,7 +81,7 @@ void always_on (int sock)
 	for(i=i+1,j=0;buffer[i]!='#';i++,j++)
 		pub_expo_str[j]=buffer[i];
 	pub_expo_str[j]='\0';
-	pub_expo = strtol(pub_expo_str,&endptr,10);	//strtol() function converts string to long integer
+	pub_expo = strtol(pub_expo_str,&endptr,10);
 	
 	for(i=i+1,j=0;i<n;i++,j++)
 		mod_str[j]=buffer[i];
@@ -89,7 +89,6 @@ void always_on (int sock)
 	mod = strtol(mod_str,&endptr,10);	
 	
 	file_name = encrypt(pub_expo,mod,requested_file); 
-	// 'file_name' contains encrypted data
 	printf("\n\nFile containing encrypted data:%s\n",file_name);
 
    fd=open(file_name,O_RDONLY);
@@ -104,22 +103,37 @@ void always_on (int sock)
 
 int main(int argc,char **argv)
 {
-int i,j;
-ssize_t n;
-FILE *fp;
-char s[80],f[80];
-struct sockaddr_in servaddr,cliaddr;
-int listenfd,connfd,clilen;
-listenfd=socket(AF_INET,SOCK_STREAM,0);
-bzero(&servaddr,sizeof(servaddr));
-servaddr.sin_family=AF_INET;
-servaddr.sin_port=htons(PORT);
-bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-listen(listenfd,1);
-clilen=sizeof(cliaddr);
-connfd=accept(listenfd,(struct sockaddr*)&cliaddr,&clilen);
-printf("client connected\n");
-always_on(connfd);
-close(listenfd);
-fclose(fp);
+    if(argc<2){
+        perror("port not provided");
+        exit(0);
+    }
+    int i,j;
+    ssize_t n;
+    FILE *fp;
+    char s[80],f[80];
+    struct sockaddr_in servaddr,cliaddr;
+    int listenfd,connfd,clilen; 
+    listenfd=socket(AF_INET,SOCK_STREAM,0);
+    if(listenfd==-1){
+	    perror("socket not created");
+	    exit(0);
+    }
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family=AF_INET;    
+    servaddr.sin_port=htons(atoi(argv[1]));
+    bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr));     
+    if(listen(listenfd,1)==-1){
+        perror("connection error");
+        exit(0);
+    }
+    clilen=sizeof(cliaddr); 
+    connfd=accept(listenfd,(struct sockaddr*)&cliaddr,&clilen);
+    if(connfd==-1){
+        perror("connection error");
+        exit(0);
+    }
+    printf("client connected\n");
+    always_on(connfd);  
+    close(listenfd);    
+    fclose(fp);
 }
